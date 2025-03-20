a1a40fe801230a3226b9b4601e627a3eb5bd2dcd
Nagul75 <nagulk697@gmail.com>
2025-03-20 11:19:55 +0530

    Set up logs UI

--- File-Monitor-Frontend/src/Components/Logs.jsx
+++ File-Monitor-Frontend/src/Components/Logs.jsx
@@ -1,17 +1,40 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material"
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import "../styles/Logs.css"
 import axios from 'axios'
 
 export default function Logs() {
+    const [logs, setLogs] = useState([])
+
     useEffect(() => {
         axios.get("http://localhost:8000/admin/logs")
             .then((res) => {
-                console.log(res)
+                setLogs(res.data.logs)
+                console.log(res.data.logs)
             })
             .catch((err) => {
                 console.log(err)
             })
     }, [])
     return(
-        <h1>Hello this is the logs page</h1>
+    <>
+        <AppBar sx={{backgroundColor: 'rgb(58,58,58)', padding: '15px'}}>
+            <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <AccountBalanceIcon fontSize="large" sx={{color: 'pink'}}></AccountBalanceIcon>
+                <Typography variant="h3">Generic Website</Typography>
+                <Typography></Typography>
+                <Button variant="text" color="inherit" size="large" href="/">Home</Button>
+            </Box>
+        </AppBar>
+
+        <Box>
+            <ul>
+                {Array.isArray(logs) && logs.map((log, index) => (
+                    <li key={index}><Typography variant="h6" color="white">{log}</Typography></li>
+                ))}
+            </ul>
+        </Box>
+    </>
     )
 }
